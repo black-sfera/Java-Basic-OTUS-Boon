6527eae1b7f0ddfdafec41241d0098b9937f4807
Виктор Бун <vintaj97@mail.ru>
2024-07-11 16:24:26 +0300

    тут сделаны первые 3 задания

--- src/main/java/ru/otus/java/basic/lesson3/Lesson3App.java
+++ src/main/java/ru/otus/java/basic/lesson3/Lesson3App.java
@@ -30,11 +30,9 @@ public class Lesson3App {
     public static void selectColor(int data){
         if (data <= 10){
             System.out.println("Красный");
-        } else {
-            if (data >= 10 && data <= 20);
+        } else if (data >= 10 && data <= 20) {
             System.out.println("Желтый");
-        } else {
-            if (data > 20);
+        } else if (data > 20){
             System.out.println("Зеленый");
         }
     }

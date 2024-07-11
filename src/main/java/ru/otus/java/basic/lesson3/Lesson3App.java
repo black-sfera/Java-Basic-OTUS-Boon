78bb5728b2b3423c62d8b478fde4095feb4f4de5
Виктор Бун <vintaj97@mail.ru>
2024-07-11 16:21:59 +0300

    тут сделаны первые 2 задания

---
+++ src/main/java/ru/otus/java/basic/lesson3/Lesson3App.java
@@ -0,0 +1,41 @@
+package ru.otus.java.basic.lesson3;
+
+public class Lesson3App {
+    public static void main(String[] args) {
+        greetings();
+        checkSing(50,20,-15);
+        checkSing(-30,10,20);
+        checkSing(15,5,-40);
+        selectColor(5);
+        selectColor(10);
+        selectColor(15);
+        selectColor(25);
+    }
+
+    public static void greetings() {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("from");
+        System.out.println("Java");
+//        System.out.println("Hello\nWorld\nfrom\nJava"); так тоже работает.
+    }
+    public static void checkSing(int a, int b, int c) {
+        int result = a + b + c;
+        if (result >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void selectColor(int data){
+        if (data <= 10){
+            System.out.println("Красный");
+        } else {
+            if (data >= 10 && data <= 20);
+            System.out.println("Желтый");
+        } else {
+            if (data > 20);
+            System.out.println("Зеленый");
+        }
+    }
+}

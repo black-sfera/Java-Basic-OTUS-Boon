fc7ea20543ed6a14a7ff2280439abe8175664bab
Виктор Бун <vintaj97@mail.ru>
2024-08-15 18:03:49 +0300

    сделал задание 1

---
+++ src/main/java/ru/otus/java/basic/lesson7/lesson7App.java
@@ -0,0 +1,24 @@
+package ru.otus.java.basic.lesson7;
+
+import java.util.Arrays;
+
+public class lesson7App {
+    public static void main(String[] args) {
+        int[][] array = {{1, 2, 3}, {0, 4, 2}};
+        int result =  sumOfPositiveElements(array);
+        System.out.println("Сумма элементов массива " + result);
+    }
+
+    public static int sumOfPositiveElements(int[][] input) {
+        int sum = 0;
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input[i].length; j++) {
+                if (input[i][j] > 0) {
+                    sum += input[i][j];
+                    System.out.println(Arrays.deepToString(input));
+                }
+            }
+        }
+        return sum;
+    }
+}
